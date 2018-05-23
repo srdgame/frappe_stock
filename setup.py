f15bd3dd5887249b47584d0b298e124dc36b7107
Dirk Chang <dirk@kooiot.com>
2018-05-23 21:27:30 +0800

    stock module

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('
-')
-
-# get version from __version__ variable in stock/__init__.py
+# get version from __version__ variable in cloud/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('stock/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+with open('cloud/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='stock',
